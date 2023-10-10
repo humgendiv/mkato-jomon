f8cafddb426457995db7ccceba5c9441652c4fc1
MasserKato <mkat0@proton.me>
2023-10-10 19:06:21 +0900

    番号の振り方をわかりやすく変更・DPでフィルターするスクリプト作成

--- 0-0-quality_check/job.sh
+++ 0-0-quality_check/job.sh
@@ -2,5 +2,5 @@ SAMPLES=("FM020" "F23" "I4" "T5")
 
 for SAMPLE in "${SAMPLES[@]}"
 do
-    qsub -v SAMPLE=$SAMPLE /home/mkato/mkato-jomon/0-quality_check/0-0-stats.sh
+    qsub -v SAMPLE=$SAMPLE /home/mkato/mkato-jomon/0-0-quality_check/0-0-stats.sh
 done
--- 0-1-preprocessing/0-1-0-filter.sh
+++ 0-1-preprocessing/0-1-0-filter.sh
@@ -1,1 +1,13 @@
-bcftools filter -e 'INFO/DP<10 || INFO/DP>79' -o filtered3.vcf filtered2.vcf
+#!/bin/bash
+#PBS -N filter_vcf
+#PBS -o output_file.log
+#PBS -e error_file.log
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=10:00:00
+
+DIR="/home1/mkato/hdd_data/data/0-0-raw_vcf"
+OUTPUT_DIR="/home1/mkato/hdd_data/data/0-1-filtered_vcf"
+
+mkdir -p $OUTPUT_DIR
+
+bcftools filter -e 'INFO/DP<10 || INFO/DP>100' -Oz -o $OUTPUT_DIR/${SAMPLE}_filtered.vcf.gz $DIR/${SAMPLE}.vcf.gz
