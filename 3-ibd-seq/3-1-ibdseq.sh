a0b51fe39b82a2d371b0ce358b08ecfe2eefd855
MasserKato <mkat0@proton.me>
2023-10-30 19:25:29 +0900

    東アジアPCAプロット・ibdseq実行スクリプト

---
+++ 3-ibd-seq/3-1-ibdseq.sh
@@ -0,0 +1,2 @@
+sample="FM020"
+java -Xmx160000m -jar /home/mkato/ibdseq.r1206.jar gt=/home/mkato/hdd_data/data/0-0-raw_vcf/${sample}.vcf.gz out=/home/mkato/hdd_data/data/3-ibd-seq/${sample} nthreads=16
