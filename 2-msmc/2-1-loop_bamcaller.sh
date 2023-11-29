7abfa46f3d2b2410a9497c974f17639d3e6ad0cb
MasserKato <mkat0@proton.me>
2023-11-29 19:37:20 +0900

    プロット用スクリプト追加

--- 2-msmc/2-1-loop_bamcaller.sh
+++ 2-msmc/2-1-loop_bamcaller.sh
@@ -1,9 +1,9 @@
 for chr in {1..22}
 do
-    #qsub -v sample_name=T5,depth=35,chr=$chr /home/mkato/mkato-jomon/2-msmc/2-0-bamcaller.sh
-    #echo "T5の${chr}"
-    #qsub -v sample_name=I4,depth=28,chr=$chr /home/mkato/mkato-jomon/2-msmc/2-0-bamcaller.sh
-    #echo "I4の${chr}"
-    qsub -v sample_name=NA18939,depth=45,chr=$chr /home/mkato/mkato-jomon/2-msmc/2-0-bamcaller.sh
-    echo "JPTの${chr}"
+    qsub -v sample_name=T5,depth=25,chr=$chr /home/mkato/mkato-jomon/2-msmc/2-0-bamcaller.sh
+    echo "T5の${chr}"
+    qsub -v sample_name=I4,depth=25,chr=$chr /home/mkato/mkato-jomon/2-msmc/2-0-bamcaller.sh
+    echo "I4の${chr}"
+    #qsub -v sample_name=NA18939,depth=45,chr=$chr /home/mkato/mkato-jomon/2-msmc/2-0-bamcaller.sh
+    #echo "JPTの${chr}"
 done
