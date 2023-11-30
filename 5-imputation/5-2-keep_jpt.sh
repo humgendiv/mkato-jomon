ed4c6868b5f26895c5359eb4b7d28d038f68e7c5
MasserKato <mkat0@proton.me>
2023-11-30 19:16:25 +0900

    jpt抽出

--- 5-imputation/5-2-keep_jpt.sh
+++ 5-imputation/5-2-keep_jpt.sh
@@ -1,1 +1,8 @@
-bcftools view -S [サンプルリストファイル名] [VCFファイル名] -o [出力ファイル名]
+suffix=".phase3_shapeit2_mvncall_integrated_v5b.20130502.genotypes.vcf.gz" ;
+dir=/home/mkato/hdd_data/data/Genomes1000/vcf/
+outdir=/home/mkato/hdd_data/data/Genomes1000/jptvcf
+
+bcftools view --force-samples -S /home/mkato/mkato-jomon/5-imputation/sample_ids.txt \
+${dir}/ALL.chr${chr}${suffix} | /usr/local/bin/bgzip -c > ${outdir}/chr${chr}_jpt.vcf.gz
+
+/usr/local/bin/tabix ${outdir}/chr${chr}_jpt.vcf.gz
