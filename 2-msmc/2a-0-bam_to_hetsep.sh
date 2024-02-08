720ef1e4e0d0bad54320214e469bac7831c468fd
MasserKato <mkat0@proton.me>
2024-02-08 15:43:33 +0900

    新たにmsmcを円滑に前処理できるスクリプトを追加

--- 2-msmc/2a-0-bam_to_hetsep.sh
+++ 2-msmc/2a-0-bam_to_hetsep.sh
@@ -1,1 +1,26 @@
-DATADIR=/
+#bamファイルが格納されているディレクトリを予め指定しておく
+DATADIR=/home1/mkato/hdd_data/data/bam/share/
+# bamファイルを指定. ファイル名はこのスクリプトの実行時に第1引数として拡張子付きで指定すること。
+BAMFILE=${DATADIR}$1
+# Depthに関する情報。このスクリプトの実行時に第2引数として指定すること。
+DEPTH=$2
+
+# まず、bamファイルの染色体に関する情報が、chrという文字列がつくかつかないかを判定する
+# chrを除去するのは時間がかかるため、for文を回す際にchrつきに対応できるようにする。
+# bamファイルの3列めの文字列にchrが含まれているかどうかを判定する。
+# samtools viewでbamファイルを読み込み、awkで3列目の文字列を取得し、grepでchrを含むかどうかを判定する。
+
+chr_or_not = samtools view $BAMFILE | head | awk '{print $3}' | grep -q "chr"
+if [ $chr_or_not -eq 0 ]; then
+	# このときchrは含まれていないので、通常のfor文で処理する
+	for i in {1..22}
+	do
+		qsub -v BAMFILE=$BAMFILE,DEPTH=$DEPTH,CHR=$i ./2a-sub-bamcaller.sh
+	done
+else
+	# このときchrは含まれているので、for文の添字にchrをつけて処理する
+	for i in {1..22}
+	do
+		qsub -v BAMFILE=$BAMFILE,DEPTH=$DEPTH,CHR=chr$i ./2a-sub-bamcaller.sh
+	done
+fi
--- 2-msmc/2a-sub-bamcaller.sh
+++ 2-msmc/2a-sub-bamcaller.sh
@@ -1,1 +1,23 @@
-#! /bin/bash
+# 染色体ごとにbamに対してmsmcを実行するための処理を実行する
+# 2a-0-bamcaller.shで回すfor文の中で実行される
+# このスクリプトは、2a-0-bamcaller.shから引数を受け取る
+BAMFILE=$BAMFILE
+DEPTH=$DEPTH
+CHROM=$CHR
+# CHROMには文字列chrが入っている場合があり、数字のみで使いたい場合のためにchrを取り除く（もともと含まれていない場合はそのまま）
+CHROM_WITHOUT_CHR=$(echo $CHROM | sed -e "s/chr//g")
+DATADIR=/home/mkato/hdd_data/data/
+REFERENCE=$DATADIR/reference/hg19.fa
+MSMC_TOOLS=/home/mkato/Repo/msmc/msmc-tools/
+# サンプルネームについて、BAMFILEのファイル名から拡張子を取り除いたものを取得する
+SAMPLENAME=$(basename $BAMFILE .bam)
+OUTDIR=$DATADIR/2-msmc/$SAMPLENAME
+mkdir -p $OUTDIR
+bcftools mpileup -q 30 -Q 30 -C 50 -Ou -r $CHROM -f $REFERENCE $BAMFILE | bcftools call -c -V indels | $MSMC_TOOLS/bamCaller.py $DEPTH $OUTDIR/$SAMPLENAME_${CHROM_WITHOUT_CHR}_mask.bed.gz | gzip -c > $OUTDIR/$SAMPLENAME_${CHROM_WITHOUT_CHR}.bcf.gz
+
+REF_MASK=$DATADIR/reference/ref_masks/hs37d5_chr${CHROM_WITHOUT_CHR}.mask.bed
+$MULTIHETSEP_OUTPUT=$OUTDIR/msmc_input_file
+# 以下はgen_multihetsetに順ずる。ただし、1個体のみに対して実行。複数個体を同一集団とみなして一括で実行する場合は未実装。
+$MSMC_TOOLS/generate_multihetsep.py --chr $CHROM --mask=$OUTDIR/$SAMPLENAME_${CHROM_WITHOUT_CHR}_mask.bed.gz --mask=$REF_MASK $OUTDIR/$SAMPLENAME_${CHROM_WITHOUT_CHR}.vcf.gz > $MULTIHETSEP_OUTPUT/$SAMPLENAME.${CHROM_WITHOUT_CHR}.multihetsep.txt
+
+
