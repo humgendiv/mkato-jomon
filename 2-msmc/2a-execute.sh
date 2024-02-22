29a5285e0bdc6f00ab090ff8f1124378127f7518
MasserKato <mkat0@proton.me>
2024-02-22 17:25:06 +0900

    msmc周辺コード整理

--- 2-msmc/2a-execute.sh
+++ 2-msmc/2a-execute.sh
@@ -1,4 +1,19 @@
-#./2a-0-bam_to_hetsep.sh F23.bam 40
-#./2a-0-bam_to_hetsep.sh I15.bam 40
-./2a-0-bam_to_hetsep.sh FM020_sorted.bam 30
-#./2a-0-bam_to_hetsep.sh NA18939.bam 40
+# 実行するサンプル名とそれに紐つくデプス（整数値）のペアをリストとして指定する
+list = (
+    "F23.bam 40"
+    "I15.bam 40"
+    "FM020_sorted.bam 30"
+    "NA18939.bam 40"
+    "Iyai4b_Dra.bam 30"
+)
+
+# リストのサンプル名とデプスを一つずつ取り出して実行
+for i in "${list[@]}"
+do
+    # サンプル名とデプスを取り出す
+    BAMFILE=$(echo $i | awk '{print $1}')
+    DEPTH=$(echo $i | awk '{print $2}')
+    # デプスを取り出す
+    # ここで実行する
+    ./2a-0-bam_to_hetsep.sh $BAMFILE $DEPTH
+done
