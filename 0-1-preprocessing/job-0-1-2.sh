fcb0aa471e02e4e8a5dbb512de9f7fb136ede945
MasserKato <mkat0@proton.me>
2024-04-18 18:38:35 +0900

    下処理のコード半自動化

--- 0-1-preprocessing/job-0-1-2.sh
+++ 0-1-preprocessing/job-0-1-2.sh
@@ -1,5 +1,5 @@
-SAMPLES=("FM020.chr1-7" "FM020.chr8-22" "F23.chr1-7" "F23.chr8-22" "Iyai4b.chr1-7" "Iyai4b.chr8-22" "Todo5.chr1-7" "Todo5.chr8-22")
-
+#SAMPLES=("FM020.chr1-7" "FM020.chr8-22" "F23.chr1-7" "F23.chr8-22" "Iyai4b.chr1-7" "Iyai4b.chr8-22" "Todo5.chr1-7" "Todo5.chr8-22")
+SAMPLES=("F23.chr8-22")
 for SAMPLE in "${SAMPLES[@]}"
 do
     qsub -v SAMPLE=$SAMPLE /home/mkato/mkato-jomon/0-1-preprocessing/0-1-2-master_bim_extract.sh
