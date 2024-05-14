f203e8ecefdfcd27e67fe62601a9d7c5e16f2917
MasserKato <mkat0@proton.me>
2024-05-14 18:02:32 +0900

    計算量を削減

--- 0-10-utils/korean/f2_new.py
+++ 0-10-utils/korean/f2_new.py
@@ -6,6 +6,7 @@ vcf_file = '/home/mkato/hdd_data/data/Genomes/korean/jomon_korean_comm/merged/m_
 
 # アリル頻度データのディレクトリ
 freq_dir = '/home/mkato/hdd_data/data/Genomes/korean/af/'
+
 # f2統計量を計算する関数
 def calculate_f2(vcf_genotype, ref, alt, freq_ref, freq_alt, freq):
     if vcf_genotype == '0/0':
@@ -29,51 +30,40 @@ def calculate_f2(vcf_genotype, ref, alt, freq_ref, freq_alt, freq):
 
     return (genotype_value - freq_value) ** 2
 
-# 個体ごとのf2統計量の合計とカウントを初期化
-f2_totals = {'FM020_FM020': 0.0, 'Iyai4b_Iyai4b': 0.0, 'Jomon_Jomon': 0.0, 'Todo5_Todo5': 0.0}
-counts = {'FM020_FM020': 0, 'Iyai4b_Iyai4b': 0, 'Jomon_Jomon': 0, 'Todo5_Todo5': 0}
+# VCFファイルをデータフレームとして読み込む
+vcf_data = pd.read_csv(vcf_file, sep='\t', comment='#', header=None)
+vcf_data.columns = ['CHROM', 'POS', 'ID', 'REF', 'ALT'] + [f'SAMPLE_{i}' for i in range(1, 5)]
 
-# 各染色体のアリル頻度ファイルを読み込む
+# 各染色体のアリル頻度ファイルを読み込み、結合する
 freq_files = glob.glob(freq_dir + 'chr*.af.txt')
+freq_data = pd.concat([pd.read_csv(file, sep='\t') for file in freq_files], ignore_index=True)
 
-# VCFファイルを行ごとに処理する
-with open(vcf_file, 'r') as vcf:
-    for line in vcf:
-        if line.startswith('#'):
-            continue  # ヘッダー行をスキップ
+# VCFデータとアリル頻度データをIDをキーとしてマージする
+merged_data = pd.merge(vcf_data, freq_data, on='ID', how='inner')
 
-        fields = line.strip().split('\t')
-        chrom, pos, id_, ref, alt = fields[0:5]
-        genotypes = fields[9:13]
-        sample_names = ['FM020_FM020', 'Iyai4b_Iyai4b', 'Jomon_Jomon', 'Todo5_Todo5']
+# 個体ごとのf2統計量の合計とカウントを初期化
+sample_names = ['FM020_FM020', 'Iyai4b_Iyai4b', 'Jomon_Jomon', 'Todo5_Todo5']
+f2_totals = {sample: 0.0 for sample in sample_names}
+counts = {sample: 0 for sample in sample_names}
 
-        # 対応するアリル頻度ファイルを探す
-        freq_file = freq_dir + f'chr{chrom}.af.txt'
-        if freq_file in freq_files:
-            freq_data = pd.read_csv(freq_file, sep='\t')
+# マージしたデータフレームに対してf2統計量を計算
+for _, row in merged_data.iterrows():
+    ref = row['REF_x']
+    alt = row['ALT_x']
+    freq_ref = row['REF_y']
+    freq_alts = row['ALT_y'].split(',')
+    freq_afs = list(map(float, row['AF'].split(',')))
 
-            # IDをキーとして一致する行を探す
-            freq_row = freq_data[freq_data['ID'] == id_]
-            if freq_row.empty:
-                continue  # 該当する行がない場合はスキップ
-
-            freq_row = freq_row.iloc[0]
-            freq_ref = freq_row['REF']
-            freq_alts = freq_row['ALT'].split(',')
-            freq_afs = list(map(float, freq_row['AF'].split(',')))
-
-            # 各個体の遺伝子型についてf2統計量を計算
-            for i, sample in enumerate(sample_names):
-                genotype = genotypes[i]
-                for freq_alt, freq in zip(freq_alts, freq_afs):
-                    f2 = calculate_f2(genotype, ref, alt, freq_ref, freq_alt, freq)
-                    if f2 is not None:
-                        f2_totals[sample] += f2
-                        counts[sample] += 1
-                        break  # 一致するアリルが見つかったら他のアリルのチェックをスキップ
+    for i, sample in enumerate(sample_names):
+        genotype = row[f'SAMPLE_{i+1}']
+        for freq_alt, freq in zip(freq_alts, freq_afs):
+            f2 = calculate_f2(genotype, ref, alt, freq_ref, freq_alt, freq)
+            if f2 is not None:
+                f2_totals[sample] += f2
+                counts[sample] += 1
+                break  # 一致するアリルが見つかったら他のアリルのチェックをスキップ
 
 # 各個体のf2統計量を計算して出力
 for sample in sample_names:
     f2_statistic = f2_totals[sample] / counts[sample] if counts[sample] > 0 else None
     print(f"{sample}のf2統計量: {f2_statistic}, 使用したSNPの数: {counts[sample]}")
-
